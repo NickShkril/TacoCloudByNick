58be8bf6f2afff17023bb2965252af78a4d173b2
NickShkril <Shkril9520@gmail.com>
2023-02-21 12:52:54 +0300

    Initial commit

---
+++ src/main/java/ru/shkril/tacocloudbynick/TacoCloudByNickApplication.java
@@ -0,0 +1,13 @@
+package ru.shkril.tacocloudbynick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudByNickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoCloudByNickApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/shkril/tacocloudbynick/TacoCloudByNickApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.shkril.tacocloudbynick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudByNickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
